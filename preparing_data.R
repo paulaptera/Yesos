718540215a6c64d329e15dd0f29895c257fd3f36
paulagg <pgg.976976@gmail.com>
2026-05-12 13:12:14 +0200

    update script

--- preparing_data.R
+++ preparing_data.R
@@ -10,15 +10,38 @@ library(here)
 protected <- st_read(here("dataset/spain_protected_areas.gpkg"))
 h.squamatum <- st_read(here("dataset/helianthemum_squamatum.gpkg"))
 study_area <- st_read(here("dataset/study_area.gpkg"))
+f.loscosii <- st_read(here("dataset/ferula_loscosii.gpkg"))
 
 # Checking CRS----
 
 st_crs(protected)
 st_crs(h.squamatum)
+st_crs(f.loscosii)
 st_crs(study_area)
 
+# Fixing errors----
+## Para evitar errores, inconsistencias o comportamientos inesperados en las operaciones espaciales
+## cuando se trabaja con coordenadas geográficas. Transforma de esférico a plano.
+sf_use_s2(FALSE)
 
-protected <- st_make_valid(protected)
 # Analysis----
+## Create distribution area for each specie
+## Tenemos que generar una capa de cuadrículas con el área de cada especie, de 100x100 metros?
+## Si cae dentro de esa cuadrícula de 100x100 un punto, mantenemos la cuadrícula,
+## así se genera una capa con la dsitribución y podemos ver mejor el área dentro de espacios protegidos.
+
 ## Which points of h.sq are inside a protected area??
 inside <- st_intersects(h.squamatum, protected)
+h.squamatum$protected <- lengths(inside) > 0
+table(h.squamatum$protected)
+mean(h.squamatum$protected)*100
+
+inside <- st_intersects(f.loscosii, protected)
+f.loscosii$protected <- lengths(inside) > 0
+table(f.loscosii$protected)
+f.loscosii_protected <- mean(f.loscosii$protected)*100
+
+## Plotting
+
+
+
